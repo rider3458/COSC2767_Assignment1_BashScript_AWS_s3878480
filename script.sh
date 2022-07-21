b25fc20ad8cdf2c94b4c35778948ab8b0a154b18
rider3458 <73817958+rider3458@users.noreply.github.com>
2022-07-21 09:29:42 +0700

    Update script.sh
    use sudo su - to keep the same env
    will add .bash_profile later to update the path

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo su
+sudo su -
 sudo amazon-linux-extras install -y java-openjdk11
 curl https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz --output /opt/maven.tar.gz
 cd /opt
@@ -10,4 +10,4 @@ mv apache-maven-3.8.6 maven
 mv apache-tomcat-9.0.64 tomcat
 ln -s /opt/tomcat/bin/startup.sh /usr/local/bin/tomcatup
 ln -s /opt/tomcat/bin/shutdown.sh /usr/local/bin/tomcatdown
-tomcatup
+/usr/local/bin/tomcatup
