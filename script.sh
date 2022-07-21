c7d8bdb1e757264db04b69c096e2beffbd193d0e
rider3458 <73817958+rider3458@users.noreply.github.com>
2022-07-21 18:22:41 +0700

    Replace newer link for tomcat

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ sudo amazon-linux-extras install -y java-openjdk11
 curl https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz --output /opt/maven.tar.gz
 cd /opt
 tar -xvzf /opt/maven.tar.gz
-curl https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.64/bin/apache-tomcat-9.0.64.tar.gz --output /opt/tomcat.tar.gz
+curl https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.22/bin/apache-tomcat-10.0.22.tar.gz --output /opt/tomcat.tar.gz
 tar -xvzf /opt/tomcat.tar.gz
 mv apache-maven-3.8.6 maven
 mv apache-tomcat-9.0.64 tomcat
